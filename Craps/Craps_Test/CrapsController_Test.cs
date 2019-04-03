7b1f9331b27209b3f1ddabda4dd09c64760da5d1
Coulter Anderson <coultermanderson@gmail.com>
2019-04-03 10:15:59 -0400

    Added further unit tests to validate the craps controllers result logic.

--- Craps/Craps_Test/CrapsController_Test.cs
+++ Craps/Craps_Test/CrapsController_Test.cs
@@ -8,10 +8,10 @@ using System.Text;
 namespace Craps_Test
 {
     [Microsoft.VisualStudio.TestTools.UnitTesting.TestClass]
-    public class MyTestClass
+    public class CrapsControllerTest
     {
         [TestMethod]
-        public void GetCrapsResult()
+        public void GetCrapsResult_Given7_expectsWin()
         {
             //Array
             CrapsController testedController = new CrapsController(new RollDataHandler());
@@ -25,6 +25,91 @@ namespace Craps_Test
 
             //Assert
             Assert.AreEqual("win", result);
+        }
+
+        [TestMethod]
+        public void GetCrapsResult_Given11_expectsWin()
+        {
+            //Array
+            CrapsController testedController = new CrapsController(new RollDataHandler());
+            CrapsResultModel inputModel = new CrapsResultModel();
+            inputModel.FirstRollValue = 6;
+            inputModel.SecondRollValue = 5;
+            string result = null;
+
+            //Act
+            result = testedController.GetCrapsResult(inputModel.RollSum, inputModel.GameResult, inputModel.Point);
+
+            //Assert
+            Assert.AreEqual("win", result);
+        }
+
+        [TestMethod]
+        public void GetCrapsResult_Given12_expectsLoss()
+        {
+            //Array
+            CrapsController testedController = new CrapsController(new RollDataHandler());
+            CrapsResultModel inputModel = new CrapsResultModel();
+            inputModel.FirstRollValue = 6;
+            inputModel.SecondRollValue = 6;
+            string result = null;
+
+            //Act
+            result = testedController.GetCrapsResult(inputModel.RollSum, inputModel.GameResult, inputModel.Point);
+
+            //Assert
+            Assert.AreEqual("loss", result);
+        }
+
+        [TestMethod]
+        public void GetCrapsResult_Given3_expectsLoss()
+        {
+            //Array
+            CrapsController testedController = new CrapsController(new RollDataHandler());
+            CrapsResultModel inputModel = new CrapsResultModel();
+            inputModel.FirstRollValue = 1;
+            inputModel.SecondRollValue = 2;
+            string result = null;
+
+            //Act
+            result = testedController.GetCrapsResult(inputModel.RollSum, inputModel.GameResult, inputModel.Point);
+
+            //Assert
+            Assert.AreEqual("loss", result);
+        }
+
+        [TestMethod]
+        public void GetCrapsResult_Given2_expectsLoss()
+        {
+            //Array
+            CrapsController testedController = new CrapsController(new RollDataHandler());
+            CrapsResultModel inputModel = new CrapsResultModel();
+            inputModel.FirstRollValue = 1;
+            inputModel.SecondRollValue = 1;
+            string result = null;
+
+            //Act
+            result = testedController.GetCrapsResult(inputModel.RollSum, inputModel.GameResult, inputModel.Point);
+
+            //Assert
+            Assert.AreEqual("loss", result);
+        }
+
+        [TestMethod]
+        public void GetCrapsResult_Given4_expectsPoint()
+        {
+            //Array
+            CrapsController testedController = new CrapsController(new RollDataHandler());
+            CrapsResultModel inputModel = new CrapsResultModel();
+            inputModel.FirstRollValue = 2;
+            inputModel.SecondRollValue = 2;
+            string result = null;
+
+            //Act
+            result = testedController.GetCrapsResult(inputModel.RollSum, inputModel.GameResult, inputModel.Point);
+
+            //Assert
+            Assert.AreEqual("point", result);
         }
     }
 }
